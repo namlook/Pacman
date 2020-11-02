e3ebab5af2161b7315aed513bfaf4d4f6cff1e7a
Namlook <n.namlook@gmail.com>
2020-11-02 12:40:21 +0100

    Updates

---
+++ precache-manifest.c5f0a3fc333d5cbb10bbb1dd4498aac4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba5ac0e25c93617243f",
+    "url": "/css/about.bccf3ab4.css"
+  },
+  {
+    "revision": "7f16e571f6f6a99c6c97",
+    "url": "/css/app.26d13a84.css"
+  },
+  {
+    "revision": "9948e8186f06e77f6316802e50d4ef40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ba5ac0e25c93617243f",
+    "url": "/js/about.c77f1052.js"
+  },
+  {
+    "revision": "7f16e571f6f6a99c6c97",
+    "url": "/js/app.850864aa.js"
+  },
+  {
+    "revision": "5d739176ebb6b6f92c19",
+    "url": "/js/chunk-vendors.249e0520.js"
+  },
+  {
+    "revision": "77c5ec9ad7ccf3285e92337b339c5d32",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
