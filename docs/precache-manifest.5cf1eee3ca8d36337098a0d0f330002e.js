9642f76e70914ae4decfec306962a88de8855524
Namlook <n.namlook@gmail.com>
2020-11-02 14:19:52 +0100

    remove docs

---
+++ docs/precache-manifest.5cf1eee3ca8d36337098a0d0f330002e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff565a0e1f8fcdf45383",
+    "url": "/css/about.bccf3ab4.css"
+  },
+  {
+    "revision": "21978a52d735c34392b3",
+    "url": "/css/app.26d13a84.css"
+  },
+  {
+    "revision": "b20462aaf337c2b816e5ad256395dd7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff565a0e1f8fcdf45383",
+    "url": "/js/about.830979ce.js"
+  },
+  {
+    "revision": "21978a52d735c34392b3",
+    "url": "/js/app.2f034ffd.js"
+  },
+  {
+    "revision": "5d739176ebb6b6f92c19",
+    "url": "/js/chunk-vendors.249e0520.js"
+  },
+  {
+    "revision": "77c5ec9ad7ccf3285e92337b339c5d32",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
